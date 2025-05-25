ea70411dafd39e14ab8ce7a2faceae34b7f71ab5
주형준 <wn1415@naver.com>
2025-05-26 00:53:03 +0900

    fix: syntax error in authApi.ts

--- src/apis/authApi.ts
+++ src/apis/authApi.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { clientId, clientSecret } from "../configs/authConfig";
 import { ClientCredentialTokenResponse } from "../models/auth";
-
+import { redirectUri } from "../configs/commonConfig";
+ 
 const encodedBase64 = (data:string):string => {
     if (typeof window !== "undefined") {
         //브라우저 환경
         return btoa(data);
-    } else{
+    } else {
         //Node.js 환경
         return Buffer.from(data).toString("base64");
     }
@@ -32,5 +33,30 @@ export const getClientCredentialToken = async (): Promise<ClientCredentialTokenR
         return response.data;
     } catch(err) {
         throw new Error("Fail to fetct client credential token");
+    }
+}
+
+export const exchangeToken = async (code:string, codeVerifier:string) => {
+    try {
+        const url = "https://accounts.spotify.com/api/token";
+        if(!clientId || !redirectUri) {
+            throw new Error("Client ID or Redirect URI is not configured");
+        }
+
+        const body = new URLSearchParams({
+            client_id: clientId,
+            grant_type: "authorization_code",
+            code,
+            redirect_uri: redirectUri,
+            code_verifier: codeVerifier,
+        });
+        const response = await axios.post(url, body, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
+        return response.data;
+    } catch(err) {
+        throw new Error("fail to fetch token");
     }
 }
